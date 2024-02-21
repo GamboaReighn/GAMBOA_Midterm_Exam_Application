76220e7d402cdcf546814e196f55c43db9737e90
GamboaReighn <jrg4876@students.uc-bcf.edu.ph>
2024-02-21 09:47:32 +0800

    Removed lines blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,10 @@
 <div>
     <!-- resources/views/products/index.blade.php -->
 
-@extends('layouts.app')
-
-@section('content')
-    <h1>{{ ucfirst($theme) }} Products</h1>
-
     <ul>
         @foreach ($products as $product)
             <li>{{ $product['title'] }} - ${{ $product['price'] }}</li>
         @endforeach
     </ul>
-@endsection
+
 </div>
