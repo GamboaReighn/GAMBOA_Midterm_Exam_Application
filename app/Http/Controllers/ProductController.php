308242241c63abc5c35738ef2e77e9f80374ef10
GamboaReighn <jrg4876@students.uc-bcf.edu.ph>
2024-02-21 09:32:09 +0800

    Created new controllerand defined products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+   public function index()
+   {
+    $products = [
+        ['The Lightning Theif' =>'Book 1', 'price' => 300],
+        ['The House of Hades' =>'Book 2', 'price' => 350],
+        ['Avatar: The Last Airbender' =>'Book 3', 'price' => 250],
+
+    ];
+    return view('products.index', compact('products'));
+   }
 }
